7869f0a390e820cef30de5628bf578e3e654a6a4
Hui Yu <ityuhui@gmail.com>
2021-01-09 16:25:26 +0800

    0109-1

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
   @Get()
   findAll(): string {
     return 'This action returns all cats';
   }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  }
 }
+
