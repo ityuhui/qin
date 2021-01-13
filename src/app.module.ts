a4ebb4cb71ed3ebd9c818adde79fca3c29b0c4aa
Hui Yu <ityuhui@gmail.com>
2021-01-13 14:24:13 +0800

    20210113-1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CatsController } from './cats.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController,CatsController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [/*AppController,*/CatsController],
   providers: [AppService],
 })
 export class AppModule {}
